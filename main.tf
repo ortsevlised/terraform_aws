bd2542f50401b20e47d60a3bc01d8b2c97a14bde
ortsevlised <jdesilvestro@gmail.com>
2024-02-14 19:24:23 +0000

    Added terraform script to: - Create a VPC with 2 public and 2 private subnets. - Create an E2C instance on one of the public networks with a basic web server running on port 8080

---
+++ main.tf
@@ -0,0 +1,113 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = {
+    Name = "Project VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-1a"
+
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-west-1b"
+
+  tags = {
+    Name = "Public Subnet 2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Project VPC IG"
+  }
+}
+
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_asso2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_security_group" "my_security_group" {
+  name   = "test_server_sg"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Access to 8080 port from outside world"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+  }
+}
+
+resource "aws_instance" "test_server" {
+  ami                    = "ami-0aef57767f5404a3c"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              echo "Hello world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "Server 1"
+  }
+}
+
